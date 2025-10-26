bcc577a3882475095608e8f024bad4c53f7d1491
PussInScreen <19553807+PussInScreen@users.noreply.github.com>
2025-10-26 20:09:30 +0000

    Beta Commit
    Fixed bugs, found more, nearly there though :)

--- app/src/main/java/com/tscireland/tscireland/MainActivity.kt
+++ app/src/main/java/com/tscireland/tscireland/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.tscireland.tscireland
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    // Suppressing as the site is rendered through Squarespace code.
+    // TODO: Review their SDLC
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Find the WebView by its unique ID
         val webView = findViewById<WebView>(R.id.web)
+
+        if (!isNetworkAvailable(applicationContext) ) { // loading offline
+            webView.getSettings().cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+        }
 
         // loading url in the WebView.
         webView.loadUrl("https://tscireland.com/")
@@ -23,5 +35,32 @@ class MainActivity : AppCompatActivity() {
         // WebViewClient allows you to handle
         // onPageFinished and override Url loading.
         webView.webViewClient = WebViewClient()
+        supportActionBar?.hide()
+    }
+    private fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nw = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            //for other device how are able to connect with Ethernet
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            //for check internet over Bluetooth
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            else -> false
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Find the WebView by its unique ID
+        val webView = findViewById<WebView>(R.id.web)
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no webpage history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
     }
 }
